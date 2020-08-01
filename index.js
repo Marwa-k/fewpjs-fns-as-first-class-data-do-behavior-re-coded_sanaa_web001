cef1e7eafc1afac30025d03276350c621c8eb008
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-08-01 17:18:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function greet(time)
 {
   const hour=parseInt(time,10);
   if(hour<12) return 'Good Morning';
-  if(hour>12) return 'Good Evening';
+  if(hour>17) return 'Good Evening';
    return 'Good Afternoon';
 }
 /* Write your implementation of displayMessage() */
